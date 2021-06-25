2ea99c146b27836af0eb0a56a0e823ac15ad2e9d
Christophe Roullier <christophe.roullier@st.com>
2021-06-25 10:17:35 +0200

    net: ethernet: stmmac: stm32: change warning to debug message
    There is a new parameter in syscon in ethernet DT since stm32mp13
    to manage several ethernet intances.
    In STM32MP15, there is a message "Warning sysconfig register mask not set"
    when this new parameter is not set.
    To avoid too many messages,replace dev_info with pr_debug to print this message.
    
    Signed-off-by: Christophe Roullier <christophe.roullier@st.com>
    Change-Id: Ie14cfa1d74dd512e84324e245708234e38cb441f
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/211916
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Alexandre TORGUE <alexandre.torgue@foss.st.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-stm32.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-stm32.c
@@ -308,7 +308,7 @@ static int stm32_dwmac_parse_data(struct stm32_dwmac *dwmac,
 	dwmac->mode_mask = SYSCFG_MP1_ETH_MASK;
 	err = of_property_read_u32_index(np, "st,syscon", 2, &dwmac->mode_mask);
 	if (err)
-		dev_info(dev, "Warning sysconfig register mask not set\n");
+		pr_debug("Warning sysconfig register mask not set\n");
 
 	return 0;
 }
